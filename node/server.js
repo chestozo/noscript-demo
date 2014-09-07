95dbc5fcd5c8d26e443f4e2451a110179d10d5b1
chestozo <chestozo@yandex-team.ru>
2014-09-07 18:42:23 +0400

    install deps and more scripts on server

--- node/server.js
+++ node/server.js
@@ -1,1 +1,13 @@
-var no = require('../node_modules/noscript/dist/noscript.module.js')();
+// <script src="/node_modules/yate/lib/runtime.js"></script>
+// <script src="/node_modules/noscript/yate/noscript-yate-externals.js"></script>
+// <script src="/templates.yate.js"></script>
+
+// FIXME global как-то не особо круто...
+var ns = global.ns = require('../node_modules/noscript/dist/noscript.module.js')();
+
+require('../js/models.js');
+require('../js/views.js');
+require('../js/layouts.js');
+require('../js/routes.js');
+
+console.log(ns.Model.get('photos').isValid());
